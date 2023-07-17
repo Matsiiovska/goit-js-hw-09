ab436c99d87756563d2d2407007e571d3acad86b
Matsiiovska <125771191+Matsiiovska@users.noreply.github.com>
2023-07-17 13:34:19 +0000

    Deploying to gh-pages from @ Matsiiovska/goit-js-hw-09@2f0f7e99c05fee6fe2c746985f9335ab6da00174 🚀

---
+++ 01-color-switcher.fe660562.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),o=document.querySelector("body"),r=0;t.addEventListener("click",(function(){r=setInterval(n,1e3),t.toggleAttribute("disabled")})),e.addEventListener("click",(function(){clearInterval(r),t.removeAttribute("disabled")}));function n(){o.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}}();
+//# sourceMappingURL=01-color-switcher.fe660562.js.map
